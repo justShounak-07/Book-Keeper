b261af228c8fb16790942b1a78eba19c5581351f
vboxuser <vboxuser@xubuntu.myguest.virtualbox.org>
2025-08-11 03:28:09 +0530

    Added the form for inserting books in library

--- library.js
+++ library.js
@@ -14,6 +14,7 @@ function addBookToLibrary(title, author, pages) {
     // pushing this to array.
 
 }
+
 // // Checking in the console new books if added (in console)- 
 // addBookToLibrary("Make time", "Jake znapp", "200");
 // addBookToLibrary("Deep work", "cal Newport", "400");
@@ -37,13 +38,40 @@ function displayBooks() {
                 <p>Pages: ${book.pages}</p>
             `;
         display.appendChild(bookcard);
-
     });
-
 }
 
-// Checking in the console new books is displayed on the web page.  
-addBookToLibrary("Make time", "Jake znapp", "200");
-addBookToLibrary("Deep work", "Cal Newport", "400");
-displayBooks(); //to print the books in the page. 
+// // Checking in the console new books is displayed on the web page.  
+// addBookToLibrary("Make time", "Jake znapp", "200");
+// addBookToLibrary("Deep work", "Cal Newport", "400");
+// displayBooks();
+// //to print the books in the page. 
 
+const form = document.querySelector(".bookForm");
+const dialog = document.getElementById("book-details");
+
+// Add book button on the page. 
+document.getElementById("addbookbutton").addEventListener("click", () => {
+    document.getElementById("book-details").showModal();
+});
+
+document.getElementById("cancel").addEventListener("click", () => {
+    document.getElementById("book-details").close();
+});
+
+document.querySelector(".bookForm").addEventListener("submit", (e) => {
+
+    e.preventDefault() //it will prevent default page load. 
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+
+    // Adding the new book to tha library. 
+    addBookToLibrary(title, author, pages);
+
+    //refresh the display by showing the output.
+    displayBooks();
+    document.getElementById("book-details").close();
+    document.querySelector(".bookForm").reset();
+}); 
