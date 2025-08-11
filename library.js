2bdd8824a0e4f464f3153d6c276db011224e3366
vboxuser <vboxuser@xubuntu.myguest.virtualbox.org>
2025-08-12 02:13:10 +0530

    made some Final changes.

--- library.js
+++ library.js
@@ -1,24 +1,28 @@
 const library = [];
 // created an empty array for storing all the books. 
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID(); //creating unique id for books. 
 }
+
+// method to toggle book status- 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+}
+
 // Creating the new Book adding- 
-function addBookToLibrary(title, author, pages) {
-    const newBook = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
     // calling new book in the constructor BOOK function. 
     library.push(newBook);
     // pushing this to array.
 
 }
 
-// // Checking in the console new books if added (in console)- 
-// addBookToLibrary("Make time", "Jake znapp", "200");
-// addBookToLibrary("Deep work", "cal Newport", "400");
-// console.log(library);
 
 
 //  to show on the page
@@ -36,16 +40,14 @@ function displayBooks() {
                 <h3>${book.title}</h3>
                 <p>Author: ${book.author}</p>
                 <p>Pages: ${book.pages}</p>
+                <p>Status: ${book.read ? 'Read' : 'Unread'}</p>
+                <button class = "toggle-btn"  data-id="${book.id}"> Toggle status </button> 
+
             `;
         display.appendChild(bookcard);
     });
 }
 
-// // Checking in the console new books is displayed on the web page.  
-// addBookToLibrary("Make time", "Jake znapp", "200");
-// addBookToLibrary("Deep work", "Cal Newport", "400");
-// displayBooks();
-// //to print the books in the page. 
 
 const form = document.querySelector(".bookForm");
 const dialog = document.getElementById("book-details");
@@ -66,12 +68,14 @@ document.querySelector(".bookForm").addEventListener("submit", (e) => {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
+    const read = document.querySelector('input[name= "read"]:checked').value === "true";
 
     // Adding the new book to tha library. 
-    addBookToLibrary(title, author, pages);
+    addBookToLibrary(title, author, pages, read);
 
     //refresh the display by showing the output.
     displayBooks();
     document.getElementById("book-details").close();
     document.querySelector(".bookForm").reset();
+
 }); 
